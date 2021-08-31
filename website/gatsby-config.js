f6df24e0daefe0c33b77cf48c283782448827950
rbiswas-mirp <rbiswas-mirp@mit.edu>
2021-08-31 22:34:12 +0530

    Update gatsby-config.js

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -3,17 +3,17 @@ const path = require('path')
 module.exports = {
   plugins: [
     {
-      resolve: 'smooth-doc',
+      resolve: 'gatsby-doc',
       options: {
-        name: 'Smooth DOC',
+        name: 'Gatsby DOC',
         siteUrl: 'https://smooth-doc.com',
         description: 'Ready to use documentation theme for Gatsby.',
         baseDirectory: path.resolve(__dirname, '../'),
-        author: 'Greg Bergé',
+        author: 'Rahul Biswas',
         sections: ['About', 'Guide', 'Components', 'Reference'],
         navItems: [{ title: 'Docs', url: '/docs/' }],
-        twitterAccount: 'neoziro',
-        githubRepositoryURL: 'https://github.com/gregberge/smooth-doc/',
+        twitterAccount: '#',
+        githubRepositoryURL: '#',
         carbonAdsURL:
           '//cdn.carbonads.com/carbon.js?serve=CE7IL2JN&placement=xstyleddev',
         docSearch: {
